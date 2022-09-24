c993960aa8b4bdaa04ef4fd716426ee9660ee301
FabiMi <91958341+FabiMi@users.noreply.github.com>
2022-09-24 09:20:54 +0200

    added the calendar module

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -87,6 +87,9 @@ module.exports.getAccessToken = async (event) => {
         // Respond with OAuth token 
         return {
           statusCode: 200,
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+          },
           body: JSON.stringify(token),
         };
       })
@@ -99,3 +102,61 @@ module.exports.getAccessToken = async (event) => {
         };
       });
   };
+
+  module.exports.getCalendarEvents = async (event) => {
+    // The values used to instantiate the OAuthClient are at the top of the file
+      const oAuth2Client = new google.auth.OAuth2(
+        client_id,
+        client_secret,
+        redirect_uris[0]
+      );
+      // Decode authorization code extracted from the URL query
+      const access_token = decodeURIComponent(`${event.pathParameters.code}`);
+      oAuth2Client.setCredentials({ access_token });
+    
+      return new Promise( (resolve, reject) => {
+
+        calendar.events.list(
+          {
+            calendarId: calendar_id,
+            auth: oAuth2Client,
+            timeMin: new Date().toISOString(),
+            singleEvents: true,
+            orderBy: "startTime",
+          },
+          (error, response) => {
+            if (error) {
+              reject(error);
+            } else {
+              resolve(response);
+            }
+          }
+        );
+
+      
+      })
+      .then( results => {
+        // Respond with OAuth token 
+        return {
+          statusCode: 200,
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+          },
+          body: JSON.stringify({ events: results.data.items }),
+        };
+      })
+      .catch((err) => {
+        // Handle error
+        console.error(err);
+        return {
+          statusCode: 500,
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+          },
+          body: JSON.stringify(error),
+    
+        
+    }}
+    
+    )};
+
