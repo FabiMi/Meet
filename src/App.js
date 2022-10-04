6d8db73050188a2ea109263c09ed977e317de7b4
FabiMi <91958341+FabiMi@users.noreply.github.com>
2022-10-04 21:12:29 +0200

    changed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import EventList from './EventList';
+import CitySearch from './CitySearch';
+
+
 
 function App() {
   return (
     <div className="App">
-     
+       <EventList />
+       <CitySearch />
+
     </div>
   );
 }
---
+++ src/CitySearch.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+class CitySearch extends Component {
+    
+    constructor() {
+        super();
+     
+        this.state = {
+          query: '',
+          suggestions: []
+        }
+      }
+
+      handleInputChanged = (event) => {
+        const value = event.target.value;
+        this.setState({ query: value });
+      }
+
+        render() {
+            return (
+            <div className="CitySearch">
+                <input
+                type="text"
+                className="city"
+                value={this.state.query}
+                onChange={this.handleInputChanged}
+                />
+                    <ul className="suggestions">
+                        {this.state.suggestions.map((suggestions) => (
+                            <li key={suggestions}>{suggestions}</li>
+                        ))}
+                        <li key='all'>
+                            <b>See all cities</b>
+                        </li>
+                    </ul>
+            </div>
+        );
+      }
+}
+
+export default CitySearch;
