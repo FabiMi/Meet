e2263d64a90ec5dc7908b2be453d5a1428d564db
FabiMi <91958341+FabiMi@users.noreply.github.com>
2022-10-08 12:28:59 +0200

    added first test

--- src/Event.js
+++ src/Event.js
@@ -1,8 +1,25 @@
 import React, { Component } from "react";
 
 class Event extends Component {
+  toggleEventDetails = () => {
+    this.setState({ show: !this.state.show });
+  };
+
+  state = { show: false };
+
   render() {
-    return <div></div>;
-  }
-}
+    const { event } = this.props;
+    return (
+        <div className="event">
+          <h1 className="event-summary-title">{event.summary}</h1>
+          </div>
+         
+         );
+        }
+      }
+
+
+
 export default Event;
+
+    
---
+++ src/_tests_/Event.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Event from '../Event';
+import { mockData } from '../mock-data';
+
+describe('<Event /> component', () => {
+  let event, EventWrapper;
+  beforeAll(() => {
+    event = mockData[0];
+
+    EventWrapper = shallow(<Event event={event} />);
+  });
+
+  test('render title in event item', () => {
+    expect(EventWrapper.find('.event-summary-title')).toHaveLength(1);
+  });
+
+});
+
