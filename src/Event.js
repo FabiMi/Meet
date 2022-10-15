89d1cbe67ff8ff2d2d9f949fadaf88161a76a878
FabiMi <91958341+FabiMi@users.noreply.github.com>
2022-10-15 09:02:20 +0200

    Added NumberOfEvents Test and more

--- src/Event.js
+++ src/Event.js
@@ -1,18 +1,32 @@
 import React, { Component } from "react";
 
 class Event extends Component {
+
+  state = { show: false };
+
   toggleEventDetails = () => {
     this.setState({ show: !this.state.show });
   };
 
-  state = { show: false };
-
   render() {
     const { event } = this.props;
     return (
-        <div className="event">
+        <div className="event" >
           <h1 className="event-summary-title">{event.summary}</h1>
-          <p className="event-info">{event.info}</p>
+          <button className="event-details-btn" onClick={this.toggleEventDetails}>details</button>
+          <div> 
+             <p className="event-info"> {event.summary} {event.location} {event.dateTime} </p> 
+          </div>
+
+         
+        
+          
+
+
+
+
+          
+
           </div>
          
          );
--- src/_tests_/Event.test.js
+++ src/_tests_/Event.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Event from '../Event';
+import EventList from '../EventList';
 import { mockData } from '../mock-data';
 
-describe('<Event /> component', () => {
+describe("<Event /> component", () => {
   let event, EventWrapper;
   beforeAll(() => {
     event = mockData[0];
@@ -19,6 +20,28 @@ describe('<Event /> component', () => {
     expect(EventWrapper.find('.event-info')).toHaveLength(1);
   });
 
+  test('render show details button in event item', () => {
+    expect(EventWrapper.find('.event-details-btn')).toHaveLength(1);
+  });
 
+  test('render event title correctly', () => {
+    expect(EventWrapper.find('.event-summary-title').text()).toBe(
+      event.summary
+  )});
+
+    test('render event info correctly', () =>  {
+        expect(EventWrapper.find('.event-info').text()).toContain (event.summary),(event.location), 
+        (event.dateTime);
+        });
+
+     test('event stays collapsed by default', () =>{
+         expect(EventWrapper.state('show')).toBe(false);
+
+     });   
+     test('render click to expand event details', () => {
+     EventWrapper.find('.event-details-btn').simulate('click');
+    expect(EventWrapper.state('show')).toBe(true);
+
+  });
 });
 
