dbc2175ecf310ae94a5c13ec056437418d549c39
FabiMi <91958341+FabiMi@users.noreply.github.com>
2022-10-15 09:29:36 +0200

    added tests

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,58 +1,30 @@
 import React, { Component } from 'react';
 
-class CitySearch extends Component {
+export class NumberOfEvents extends Component {
+  handleInputChanged = (event) => {
+    this.props.updateEvents(undefined, event.target.value);
+    const value = event.target.value;
+    this.setState({ numOfEvents: value });
+  };
+
     
-    constructor() {
-        super();
-     
-        this.state = {
-          query: '',
-          suggestions: []
-        }
-      }
 
-      handleInputChanged = (event) => {
-        const value = event.target.value;
-        const suggestions = this.props.locations.filter((location) => {
-          return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
-        });
-        this.setState({
-          query: value,
-          suggestions
-        });
-      };
-      
-      handleItemClicked = (suggestion) => {
-        this.setState({
-          query: suggestion
-        });
-      }
-
-        render() {
-            return (
-            <div className="CitySearch">
-                <input
-                type="text"
-                className="city"
-                value={this.state.query}
-                onChange={this.handleInputChanged}
-                />
-                    <ul className="suggestions">
-                        {this.state.suggestions.map((suggestion) => (
-                             
-                             <li
-                              key={suggestion}
-                              onClick={() => this.handleItemClicked(suggestion)}
-                            >{suggestion}</li>
-                        
-                        ))}
-                        <li key='all'>
-                            <b>See all cities</b>
-                        </li>
-                    </ul>
-            </div>
-        );
-      }
+  
+  state = { numOfEvents: 32 };
+  render() {
+    return (
+      <div className="numberOfEvents">
+        <label>
+          Number of Events:
+          <input
+            type="number"
+            className="number-input"
+            value={this.state.numOfEvents}
+            onChange={this.handleInputChanged}
+          />
+        </label>
+      </div>
+    );
+  }
 }
-
-export default CitySearch;
+export default NumberOfEvents;
