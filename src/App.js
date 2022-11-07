35776f85812e3a2d7db1cbbf8bae4ae567ddb0b6
FabiMi <91958341+FabiMi@users.noreply.github.com>
2022-11-07 09:17:34 +0100

    added

--- src/App.js
+++ src/App.js
@@ -4,50 +4,70 @@ import EventList from './Eventlist';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
 import { getEvents, extractLocations } from './api';
+import "./nprogress.css";
+
 
 
 
 class App extends Component {
   state = {
     events: [],
-    locations: []
-  }
+    locations: [],
+    numOfEvents: 20, 
+      }
 
-  updateEvents = (location) => {
-    getEvents().then((events) => {
-      const locationEvents = (location === 'all') ?
-        events :
-        events.filter((event) => event.location === location);
-      this.setState({
-        events: locationEvents
-      });
-    });
-  }
-    componentDidMount() {
-      this.mounted = true;
-      getEvents().then((events) => {
-        if (this.mounted) {
-          this.setState({ events, locations: extractLocations(events) });
-        }
-      });
-    }
+      updateEvents = (location, eventCount) => {
+        getEvents().then((events) => {
+          const locationEvents =
+            location === "all"
+              ? events
+              : events.filter((event) => event.location === location);
+          this.setState({
+            events: locationEvents.slice(0, this.state.numOfEvents),
+          });
+        });
+      };
+    
+      updateNumberOfEvents(number) {
+        this.setState({
+          numOfEvents: number,
+        });
+      }
+      componentDidMount() {
+        this.mounted = true;
+        getEvents().then((events) => {
+          if (this.mounted) {
+            this.setState({
+              events: events.slice(0, this.state.numOfEvents),
+              locations: extractLocations(events),
+            });
+          }
+        });
+      }
   
     componentWillUnmount(){
       this.mounted = false;
     }
  
-  render() {
-    return (
-      <div className="App">
-       
-        <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
-        <NumberOfEvents />
-        <EventList events={this.state.events} />
-        
-      </div>
-    );
+    render() {
+      return (
+        <div className="App">
+          <div className="filters">
+            <CitySearch
+              locations={this.state.locations}
+              updateEvents={this.updateEvents}
+            />
+            <NumberOfEvents
+              num={this.state.numOfEvents}
+              updateNumberOfEvents={(num) => this.updateNumberOfEvents(num)}
+            />
+          </div>
+          <EventList events={this.state.events} />
+        </div>
+      );
+    }
   }
-}
+  
 
 
 export default App;
--- src/CitySearch.js
+++ src/CitySearch.js
@@ -26,29 +26,38 @@ class CitySearch extends Component {
   
     this.props.updateEvents(suggestion);
   }
-  
 
   render() {
     return (
       <div className="CitySearch">
         <input
-  type="text"
-  className="city"
-  value={this.state.query}
-  onChange={this.handleInputChanged}
-  onFocus={() => { this.setState({ showSuggestions: true }) }}
-/>
-<ul className="suggestions" style={this.state.showSuggestions ? {}: { display: 'none' }}>
-  {this.state.suggestions.map((suggestion) => (
-    <li key={suggestion}>{suggestion}</li>
-  ))}
-  <li onClick={() => this.handleItemClicked("all")}>
-  <b>See all cities</b>
-</li>
-</ul>
+          type="text"
+          className="city"
+          value={this.state.query}
+          onChange={this.handleInputChanged}
+          placeholder="Search for location..."
+          onFocus={() => {
+            this.setState({ showSuggestions: true });
+          }}
+        />
+        <ul
+          className="suggestions"
+          style={this.state.showSuggestions ? {} : { display: "none" }}
+        >
+          {this.state.suggestions.map((suggestion) => (
+            <li
+              key={suggestion}
+              onClick={() => this.handleItemClicked(suggestion)}
+            >
+              {suggestion}
+            </li>
+          ))}
+          <li onClick={() => this.handleItemClicked("all")}>
+            <b>See all cities</b>
+          </li>
+        </ul>
       </div>
     );
   }
 }
-
 export default CitySearch;
