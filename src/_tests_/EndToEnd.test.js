6296912b845d25ae1f23f00ea14f64cc674588b6
FabiMi <91958341+FabiMi@users.noreply.github.com>
2023-02-28 12:30:27 +0100

    changed

--- src/_tests_/EndToEnd.test.js
+++ src/_tests_/EndToEnd.test.js
@@ -9,7 +9,7 @@ describe('show/hide an event details', () => {
     jest.setTimeout(100000);
     browser = await puppeteer.launch({
       headless: false,
-      slowMo: 250, // slow down by 250ms
+     // slowMo: 250, // slow down by 250ms
      ignoreDefaultArgs: ['--disable-extensions'] // ignores default setting that causes timeout errors
     });
     page = await browser.newPage();
