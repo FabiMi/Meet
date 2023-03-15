788666b66e9393a97c0d33a9c7520f4a6a9eb5e4
FabiMi <91958341+FabiMi@users.noreply.github.com>
2023-03-15 12:13:19 +0100

    changed

--- src/App.js
+++ src/App.js
@@ -5,43 +5,42 @@ import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
 import { getEvents, extractLocations } from './api';
 import "./nprogress.css";
-
-
-
-
-
 class App extends Component {
   state = {
     events: [],
     locations: [],
     numOfEvents: 32,
     mockEvents: []
+  }
 
-  
+  updateEvents = (location, eventCount) => {
+    getEvents().then((events) => {
+      let locationEvents = events;
+      // filter event list by location
+      if (location !== undefined) {
+        if (location !== "all") {
+          locationEvents = events.filter((event) => event.location === location);
+        }
       }
-      updateEvents = (location, numOfEvents) => {
-        getEvents().then((events) => {
-          const locationEvents =
-            location === "all"
-              ? events
-              : events.filter((event) => event.location === location);
-          this.setState({
-            events: locationEvents.slice(0, numOfEvents || this.state.numOfEvents),
-          });
-        });
-      };
-      
-      
-    
-      updateNumberOfEvents = (value) => {
-        this.setState({
-          numOfEvents: value
-        });
-      };
-      componentDidMount() {
-        this.mounted = true;
-        getEvents()
-  .then((events) => {
+
+      this.setState({
+        // Shorten event list
+        events: locationEvents.slice(0, eventCount),
+        numOfEvents: eventCount,
+      });
+    });
+  };
+
+  updateNumberOfEvents = (value) => {
+    this.setState({
+      numOfEvents: value
+    });
+  };
+
+  componentDidMount() {
+    this.mounted = true;
+    getEvents()
+    .then((events) => {
     if (this.mounted) {
       this.setState({
         events: events.slice(0, this.state.numOfEvents),
@@ -52,34 +51,32 @@ class App extends Component {
   .catch((error) => {
     console.error(error);
   });
-}
-
-  
-    componentWillUnmount(){
-      this.mounted = false;
-    }
- 
-    render() {
-      return (
-        <div className="App">
-          <div className="filters">
-            <CitySearch
-              locations={this.state.locations}
-              updateEvents={this.updateEvents}
-            />
-            <NumberOfEvents
-              num={this.state.numOfEvents}
-              updateNumberOfEvents={(num) => this.updateNumberOfEvents(num)}
-              updateEvents={this.updateEvents}
-
-            />
-          </div>
-          <EventList events={this.state.events} />
-        </div>
-      );
-    }
   }
-  
 
+
+  componentWillUnmount(){
+    this.mounted = false;
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="filters">
+          <CitySearch
+            locations={this.state.locations}
+            updateEvents={this.updateEvents}
+          />
+          <NumberOfEvents
+            num={this.state.numOfEvents}
+            updateNumberOfEvents={(num) => this.updateNumberOfEvents(num)}
+            updateEvents={this.updateEvents}
+
+          />
+        </div>
+        <EventList events={this.state.events} />
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,29 +1,36 @@
 import React, { Component } from 'react';
 import { ErrorAlert } from './Alert';
 
-export class NumberOfEvents extends Component {
-
-  state = { numOfEvents: 32, errorText: '' };
+class NumberOfEvents extends Component {
+  state = {
+    errorText: "",
+    numOfEvents: this.props.numOfEvents,
+  };
 
   handleInputChanged = (event) => {
-    const value = event.target.value;
-    if (value < 1 || value > 32) {
-      this.setState({ errorText: 'Please enter a number between 1 and 32' });
-    } else {
-      this.setState({ errorText: '', numOfEvents: value });
-      this.props.updateEvents(undefined, value);
+    let numOfEvents = event.target.value;
+    const errorText =
+      numOfEvents < 0 || numOfEvents > 32 || !numOfEvents
+        ? "Select a number from 1 to 32"
+        : "";
+    this.setState({
+      errorText,
+      numOfEvents,
+    });
+    if (!errorText) {
+      this.props.updateEvents(undefined, numOfEvents);
     }
-  }
+  };
 
   render() {
     return (
       <div className="number-of-events">
-        <ErrorAlert text={this.state.errorText} />
+        <ErrorAlert id="errorAlert" text={this.state.errorText} />
         <label>
-          Number of Events:
           <input
             type="number"
             className="number-input"
+            min={0}
             value={this.state.numOfEvents}
             onChange={this.handleInputChanged}
           />
