f469c5023ab86fe632a0cff909811633693a7d46
FabiMi <91958341+FabiMi@users.noreply.github.com>
2023-05-07 12:45:34 +0200

    set jest Timeout to 1 min

--- src/_tests_/EndToEnd.test.js
+++ src/_tests_/EndToEnd.test.js
@@ -3,15 +3,20 @@ import puppeteer from 'puppeteer';
 describe('show/hide an event details', () => {
     let browser;
     let page;
+
     beforeAll(async () => {
-        jest.setTimeout(90000);
+        jest.setTimeout(60000); // set timeout to 60 seconds
+        console.log('Launching browser...');
         browser = await puppeteer.launch({
             headless: false,
             slowMo: 250,
             ignoreDefaultArgs: ['--disable-extensions']
         });
+        console.log('Opening new page...');
         page = await browser.newPage();
-        await page.goto('http://localhost:3000/');
+        console.log('Navigating to URL...');
+        await page.goto('http://localhost:3000/meet');
+        console.log('Waiting for event selector...');
         await page.waitForSelector('.event');
     });
 
@@ -26,12 +31,14 @@ describe('show/hide an event details', () => {
 
     test('User can expand an event to see its details', async () => {
         await page.click('.event .event-details-btn');
+        await page.waitForTimeout(5000); // add a delay of 500 ms
         const eventDetails = await page.$('.event .event__Details');
         expect(eventDetails).toBeDefined();
     });
 
     test('User can collapse an event to hide its details', async () => {
         await page.click('.event .event-details-btn');
+        await page.waitForTimeout(5000); // add a delay of 500 ms
         const eventDetails = await page.$('.event .event__Details');
         expect(eventDetails).toBeNull();
     });
