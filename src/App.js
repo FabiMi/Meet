4ee183302d2935f52ab6a6609033cd659c6fc882
FabiMi <91958341+FabiMi@users.noreply.github.com>
2023-05-08 19:22:30 +0200

    added OfflineAlert

--- src/App.js
+++ src/App.js
@@ -2,15 +2,23 @@ import React, { Component } from "react"
 import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
-import NumberOfEvents from './NumberOfEvents';
-import { getEvents, extractLocations } from './api';
+import NumberOfEvents from './NumberOfEvents'
 import "./nprogress.css";
+import {OfflineAlert} from './Alert';
+import WelcomeScreen from './WelcomeScreen';
+import { getEvents, extractLocations, checkToken, getAccessToken } from
+'./api';
+import {
+  ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip} from 'recharts';
+
+
 class App extends Component {
   state = {
     events: [],
     locations: [],
     numOfEvents: 32,
-    mockEvents: []
+    mockEvents: [],
+    showWelcomeScreen: undefined
   }
 
   updateEvents = (location, eventCount) => {
@@ -37,30 +45,44 @@ class App extends Component {
     });
   };
 
-  componentDidMount() {
+  async componentDidMount() {
     this.mounted = true;
-    getEvents()
-    .then((events) => {
-    if (this.mounted) {
-      this.setState({
-        events: events.slice(0, this.state.numOfEvents),
-        locations: extractLocations(events),
-      });
-    }
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+    const accessToken = localStorage.getItem('access_token');
+    const isTokenValid = (await checkToken(accessToken)).error ? false :
+  true;
+    const searchParams = new URLSearchParams(window.location.search);
+   
+  const code = searchParams.get("code");
+    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
+    if ((code || isTokenValid) && this.mounted) {
+      getEvents().then((events) => {
+        if (this.mounted) {
+          this.setState({ events, locations: extractLocations(events) });
+        }
+  }); }
   }
-
+  
+  promptOfflineWarning = () => {
+    if (!navigator.onLine) {
+      this.setState({
+        offlineText: 'You are offline, so events may not be up to date'
+      })
+    }
+  }
 
   componentWillUnmount(){
     this.mounted = false;
   }
 
+  
+
   render() {
+    if (this.state.showWelcomeScreen === undefined) return <div
+className="App" />
+
     return (
       <div className="App">
+        <OfflineAlert text={this.state.offlineText} />
         <div className="filters">
           <CitySearch
             locations={this.state.locations}
@@ -69,13 +91,35 @@ class App extends Component {
           <NumberOfEvents
             numOfEvents={this.state.numOfEvents}
             updateEvents={this.updateEvents}
-
           />
         </div>
+
+        
+       {/*  <h4>Events in each city</h4>
+
+         <ScatterChart
+          width={400}
+          height={400}
+          margin={{
+            top: 20, right: 20, bottom: 20, left: 20,
+          }}
+        >
+          <CartesianGrid />
+          <XAxis type="number" dataKey="x" name="stature" unit="cm" />
+          <YAxis type="number" dataKey="y" name="weight" unit="kg" />
+          <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+          <Scatter name="A school" data={data} fill="#8884d8" />
+        </ScatterChart>
+*/}
+
+
         <EventList events={this.state.events} />
+        <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen}
+getAccessToken={() => { getAccessToken() }} />
       </div>
     );
   }
+
 }
 
 export default App;
