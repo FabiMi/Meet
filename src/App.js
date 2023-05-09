82c2c77ffae1171c95076c5d219aba1ee1cd4921
FabiMi <91958341+FabiMi@users.noreply.github.com>
2023-05-09 20:37:41 +0200

    added changes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import WelcomeScreen from './WelcomeScreen';
 import { getEvents, extractLocations, checkToken, getAccessToken } from
 './api';
 import {
-  ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip} from 'recharts';
+  ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer} from 'recharts';
 
 
 class App extends Component {
@@ -31,12 +31,12 @@ class App extends Component {
           locationEvents = events.filter((event) => event.location === location);
         }
       }
-
       this.setState({
         // Shorten event list
         events: locationEvents.slice(0, eventCount),
         numOfEvents: eventCount,
       });
+      this.promptOfflineWarning();
     });
   };
 
@@ -48,25 +48,39 @@ class App extends Component {
 
   async componentDidMount() {
     this.mounted = true;
-    const accessToken = localStorage.getItem('access_token');
-    const isTokenValid = (await checkToken(accessToken)).error ? false :
-  true;
+    const accessToken = localStorage.getItem("access_token");
+    const isTokenValid =
+      !window.location.href.startsWith("http://localhost") &&
+        !(accessToken && !navigator.onLine) &&
+        (await checkToken(accessToken)).error
+        ? false
+        : true;
     const searchParams = new URLSearchParams(window.location.search);
-   
-  const code = searchParams.get("code");
-    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
-    if ((code || isTokenValid) && this.mounted) {
+    const code = searchParams.get("code");
+    const byPassWelcomeScreen = code || isTokenValid;
+
+    this.setState({ showWelcomeScreen: !byPassWelcomeScreen });
+
+    if (byPassWelcomeScreen) {
       getEvents().then((events) => {
         if (this.mounted) {
-          this.setState({ events, locations: extractLocations(events) });
+          this.setState({
+            events: events.slice(0, 32),
+            locations: extractLocations(events),
+          });
         }
-  }); }
+      });
+    }
   }
   
   promptOfflineWarning = () => {
     if (!navigator.onLine) {
       this.setState({
         offlineText: 'You are offline, so events may not be up to date'
+      })
+    } else {
+      this.setState({
+        offlineText: ''
       })
     }
   }
@@ -75,56 +89,98 @@ class App extends Component {
     this.mounted = false;
   }
 
+  getData = () => {
+    const {locations, events} = this.state;
+    const data = locations.map((location)=>{
+      const number = events.filter((event) => event.location === location).length
+      const city = location.split(', ').shift()
+      return {city, number};
+    })
+    return data;
+  };
+
   
 
+  async componentDidMount() {
+    this.mounted = true;
+    const accessToken = localStorage.getItem("access_token");
+    const isTokenValid =
+      !window.location.href.startsWith("http://localhost") &&
+        !(accessToken && !navigator.onLine) &&
+        (await checkToken(accessToken)).error
+        ? false
+        : true;
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = searchParams.get("code");
+    const byPassWelcomeScreen = code || isTokenValid;
+
+    this.setState({ showWelcomeScreen: !byPassWelcomeScreen });
+
+    if (byPassWelcomeScreen) {
+      getEvents().then((events) => {
+        if (this.mounted) {
+          this.setState({
+            events: events.slice(0, 32),
+            locations: extractLocations(events),
+          });
+        }
+      });
+    }
+  }
+
+
   render() {
-    if (this.state.showWelcomeScreen === undefined) return <div
-className="App" />
+    if (this.state.showWelcomeScreen === undefined) {
+      return <div className="App" />
+    } else if (this.state.showWelcomeScreen === true) {
+      return <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen}
+        getAccessToken={() => { getAccessToken() }} />
+    } else {
+      return (
+        <div className="App">
+          <OfflineAlert text={this.state.offlineText} />
+          <h1>Meet App</h1>
+          <h2>Search for your nearest city </h2>
+          <div className="filters">
+            <CitySearch
+              locations={this.state.locations}
+              updateEvents={this.updateEvents}
+            />
 
-    return (
-      <div className="App">
-        <OfflineAlert text={this.state.offlineText} />
-        <h1>Meet App</h1>
-        <h2>Search for your nearest city </h2>
-        <div className="filters">
-          <CitySearch
-            locations={this.state.locations}
-            updateEvents={this.updateEvents}
-          />
-
-          <h3>Number of events:</h3>
-          <NumberOfEvents
-            numOfEvents={this.state.numOfEvents}
-            updateEvents={this.updateEvents}
-          />
-        </div>
+            <h3>Number of events:</h3>
+            <NumberOfEvents
+              numOfEvents={this.state.numOfEvents}
+              updateEvents={this.updateEvents}
+            />
+          </div>
 
         <h4>Events in each City</h4>:
-       {/*  <h4>Events in each city</h4>
-
-         <ScatterChart
-          width={400}
-          height={400}
-          margin={{
-            top: 20, right: 20, bottom: 20, left: 20,
-          }}
-        >
-          <CartesianGrid />
-          <XAxis type="number" dataKey="x" name="stature" unit="cm" />
-          <YAxis type="number" dataKey="y" name="weight" unit="kg" />
-          <Tooltip cursor={{ strokeDasharray: '3 3' }} />
-          <Scatter name="A school" data={data} fill="#8884d8" />
-        </ScatterChart>
-*/}
-
+  
+        <div className="data-vis-wrapper">
+         
+        <ResponsiveContainer height={400} >
+          <ScatterChart margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
+            <CartesianGrid />
+            <XAxis type="category" dataKey="city" name="city" />
+            <YAxis
+              allowDecimals={false}
+              type="number"
+              dataKey="number"
+              name="number of events"
+            />
+            <Tooltip cursor={{ strokeDasharray: "3 3" }} />
+            <Scatter data={this.getData()} fill="#8884d8" />
+          </ScatterChart>
+        </ResponsiveContainer>
+      </div>
 
         <EventList events={this.state.events} />
-        <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen}
-getAccessToken={() => { getAccessToken() }} />
+
+       
       </div>
     );
   }
 
-}
+}}
 
 export default App;
